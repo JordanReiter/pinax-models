f857d65b8d04ec8bef6532edd30e7d5b543ea9fe
Jordan Reiter (dev.editlib.org) <jordan@aace.org>
2019-05-03 17:21:41 -0400

    Restore shortcuts to LogicalDeleteModel and LogicalDeleteModelAdmin

--- pinax/models/__init__.py
+++ pinax/models/__init__.py
@@ -1,3 +1,6 @@
 import pkg_resources
 
 __version__ = pkg_resources.get_distribution("pinax-models").version
+
+from .admin import LogicalDeleteModelAdmin  # noqa
+from .models import LogicalDeleteModel  # noqa
